27667f057a04b8a860e472cd85d0a7457946027a
danialdanish0246-glitch <danialdanish0246@gmail.com>
2026-03-12 18:53:17 +0800

    Create kira_zakat.py

---
+++ kira_zakat.py
@@ -0,0 +1,7 @@
+print("----------KAUTER KIRA KIRA ZAKAT----------")
+nama = input("MASUKKAN NAMA  :")
+gaji_bulanan = int(input("MASUKKAN GAJI BULANAN  :"))
+if gaji_bulanan < 2500 :
+   print("tak payah bayar zakat.")
+else :
+   print("baru kena bayar.")
