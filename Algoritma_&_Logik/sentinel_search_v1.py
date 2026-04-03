e86bca965462ea6af99d503a912d0473083852d4
danialdanish0246-glitch <danialdanish0246@gmail.com>
2026-04-03 17:29:36 +0800

    Create sentinel_search_v1.py

---
+++ Algoritma_&_Logik/sentinel_search_v1.py
@@ -0,0 +1,27 @@
+# ============================================================
+# AUTHOR: Danial
+# Beginner level: Exploring Sentinel Search algorithm.
+# Just started learning Python. Still practicing my logic!
+# PROJECT: Sentinel Search Algorithm
+# ============================================================
+def sentinel_search(senarai,target) :
+  n = len(senarai)
+
+  elemen_terakhir = senarai[n - 1]
+  senarai[n -1] = target
+  i = 0
+  while senarai[i] != target :
+    i += 1
+  senarai[n - 1] = elemen_terakhir 
+  if (i < n - 1) or (senarai[n - 1] == target) :
+    return print(f"dh jumpa,berada dekat senarai ke-{i}")
+  else :
+    return print("tak jumpa")
+berapa_nk_masuk = int(input("nak masuk berapa nombor : "))
+nombor_list = []
+for i in range(berapa_nk_masuk) :
+    nombor = int(input(f"masukkan nombor ke-{i} :"))
+    nombor_list.append(nombor)
+cari = int(input("masukkan nombor nk cari : "))
+result = sentinel_search(nombor_list,cari)
+print(result)
